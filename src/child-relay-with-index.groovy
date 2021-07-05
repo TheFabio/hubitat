29912e6ebe30b2abc78ca66ff4aceaf076c1e8aa
TheFabio <13587934+TheFabio@users.noreply.github.com>
2021-07-05 18:14:51 +1000

    Setting up
    * adding the first version of the hhc-n-8180 Driver

---
+++ src/child-relay-with-index.groovy
@@ -0,0 +1,44 @@
+metadata {
+  definition (
+    author: "The Fabio",
+    description: "Device to raise on-off clicked events on parent devices",
+    importUrl: "https://raw.githubusercontent.com/TheFabio/hubitat/main/src/child-relay-with-index.groovy",
+    name: "child-relay-switch-with-index",
+    namespace: "thefabio"
+  ) {
+     capability "RelaySwitch"
+
+     attribute "relayIndex", "number"
+  }
+  preferences {
+  }
+}
+
+// default methods - begin
+
+// This method is called when the device is first created and can be used to initialize any device specific configuration and setup
+def installed() {
+}
+
+// This method is called when the device is removed to allow for any necessary cleanup.
+def uninstalled() {
+}
+
+// This method is called when the preferences of a device are updated.
+def updated() {
+}
+
+//This method is called in response to a message received by the device driver
+def parse(String description) {
+}
+// default methods - end
+
+//capability "RelaySwitch" - begin
+def on() {
+    getParent().switchRelay(relayIndex, true)
+}
+
+def off() {
+    getParent().switchRelay(relayIndex, false)
+}
+//capability "RelaySwitch" - end
